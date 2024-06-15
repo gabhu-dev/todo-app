7b468a6cdde3cc5b8da96f9656663917bd25d634
gabhu-dev <gabh@hotmail.es>
2024-06-14 21:07:48 -0500

    [add] CardTask

---
+++ src/@types/task.ts
@@ -0,0 +1,6 @@
+export enum TaskStatus {
+  Todo = 1,
+  InProgress = 2,
+  IsCompleted = 3,
+  IsExpired = 4,
+}
---
+++ src/components/CardTask.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { TaskStatus } from "@/@types/task"
+import { StarIcon, TrashIcon, CheckCircleIcon, CalendarIcon } from "@heroicons/vue/24/outline"
+
+defineProps<{
+  title: string
+  status: TaskStatus
+  date: string
+  starred?: boolean
+}>()
+</script>
+
+<template>
+  <div class="shadow-sm rounded border border-gray-700 border-solid h-max w-48 p-5">
+    <p class="text-xs text-gray-500 mb-2">Vencimiento: Mañana a las 4:50pm</p>
+    <h5 class="text-xl font-medium mb-4">
+      {{ title }}
+    </h5>
+    <div class="space-x-4 flex flex-row justify-end">
+      <StarIcon class="h-4 w-4 text-gray-600 cursor-pointer" />
+      <TrashIcon class="h-4 w-4 text-gray-600 cursor-pointer" />
+      <CalendarIcon class="h-4 w-4 text-gray-600 cursor-pointer" />
+      <CheckCircleIcon class="h-4 w-4 text-gray-600 cursor-pointer" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
