5b65d331369595659c1e54dd3b9183b6143da934
gabhu-dev <gabh@hotmail.es>
2024-06-17 16:16:52 -0500

    [add] title

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -2,6 +2,7 @@
 import Panel from "@/components/Panel.vue"
 </script>
 <template>
+  <h4 class="text-3xl font-semibold mt-10 mb-6">MI TODO LIST</h4>
   <div class="grid grid-cols-12 gap-3">
     <Panel v-for="i in 4" :key="i" class="grid col-span-3" />
   </div>
