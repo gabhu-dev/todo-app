8def7cba03401c8a234bca0468621e30207d7f98
gabhu-dev <gabh@hotmail.es>
2024-06-17 16:20:47 -0500

    [add] Filters

---
+++ src/components/Filters.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TButton from "@/components/TButton.vue"
+import { ButtonType } from "@/@types/button"
+</script>
+<template>
+  <div class="flex justify-end">
+    <TButton :type="ButtonType.Primary"> Agregar tarea </TButton>
+  </div>
+</template>
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import Panel from "@/components/Panel.vue"
+import Filters from "@/components/Filters.vue"
 </script>
+
 <template>
   <h4 class="text-3xl font-semibold mt-10 mb-6">MI TODO LIST</h4>
+  <Filters />
   <div class="grid grid-cols-12 gap-3">
     <Panel v-for="i in 4" :key="i" class="grid col-span-3" />
   </div>
