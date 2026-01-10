2ab831e86caf6770ce7711afd222bd9fd90ceb45
gabhu-dev <gabhu@hotmail.es>
2026-01-10 10:20:54 -0500

    [update] styles for card task

--- src/components/atoms/TDate.vue
+++ src/components/atoms/TDate.vue
@@ -21,7 +21,7 @@ const updateValue = (e: Event) => {
     <input
       type="date"
       :id="name"
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm outline-none rounded-lg focus:ring-blue-500 focus:border-blue-500 focus-visible:border-blue-500 block w-full p-2.5"
+      class="bg-white border border-gray-300 text-gray-900 text-sm outline-none rounded-lg focus:ring-blue-500 focus:border-blue-500 focus-visible:border-blue-500 block w-full p-2.5"
       :placeholder="placeholder"
       :required="required"
       @change="updateValue"
