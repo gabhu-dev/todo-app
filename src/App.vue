fcb4db331dcc36871cf2c70408e23821d96b52e6
gabhu-dev <gabhu@hotmail.es>
2026-02-07 21:10:18 -0500

    [add ]Implement initial Vue Router setup, including dependency, configuration, and a basic home view.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import MainSection from "@/components/MainSection.vue"
 import MainLayout from "@/layouts/MainLayout.vue"
 </script>
 
 <template>
   <MainLayout>
-    <MainSection />
+    <router-view />
   </MainLayout>
 </template>
 
